b13fe4dd654a678a5aa490ff6c5c26a3999e0a80
fatihatuzjohora <fatihatuzjohora@gmail.com>
2024-03-16 21:45:00 +0600

    first commit

---
+++ src/Component/navber/navber.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const navber = () => {
+    return (
+        <div>
+            <div>
+
+            </div>
+
+            <div>
+
+            </div>
+            
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default navber;
